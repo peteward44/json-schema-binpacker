8195f68ae59f70a200773f3b2bc4d7203417adc3
Pete Ward <peteward44@gmail.com>
2018-02-08 22:13:09 +0000

    initial commit

---
+++ test/index.js
@@ -0,0 +1,29 @@
+const { assert } from 'chai';
+const binPacker = require( '..' );
+
+describe( 'basic', () => {
+	it.skip( 'compress', () => {
+		const schema = {
+			
+		};
+		
+		const object = {
+			
+		};
+		
+		const buffer = binPacker.compress( schema, object );
+		
+	} );
+	
+	it.skip( 'decompress', () => {
+		
+	} );
+	
+	it.skip( 'compress and decompress', () => {
+		
+	} );
+	
+	it.skip( 'compress and decompress with different schema will fail', () => {
+		
+	} );
+} );
