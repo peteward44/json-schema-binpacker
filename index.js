98b486dc49c7872b9ceefa27f9d9bd3d1b68b9f3
Pete Ward <peteward44@gmail.com>
2018-02-09 22:32:57 +0000

    refactored out buffer to io file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const limits = require( './limits.js' );
+const io = require( './io.js' );
 
 function joinPath( ...args ) {
 	let result = '';
@@ -11,38 +11,6 @@ function joinPath( ...args ) {
 	return result;
 }
 
-function shouldUseFloat( schema ) {
-	if ( typeof schema.minimum === 'number' && typeof schema.maximum === 'number' ) {
-		return schema.minimum >= limits.FLT_MIN && schema.maximum <= limits.FLT_MAX;
-	}
-	return false;
-}
-
-function getNumberType( schema ) {
-	if ( typeof schema.minimum === 'number' && typeof schema.maximum === 'number' ) {
-		// TODO: store ranges instead of integers to reduce sizes further
-		if ( schema.minimum < 0 ) {
-			// signed
-			if ( schema.minimum >= -0x7F && schema.maximum <= 0x7F ) {
-				return "int8";
-			} else if ( schema.minimum >= -0x7FFF && schema.maximum <= 0x7FFF ) {
-				return "int16";
-			} else {
-				return "int32";
-			}
-		} else {
-			// unsigned
-			if ( schema.maximum > 0xFFFF ) {
-				return "uint32";
-			} else if ( schema.maximum > 0xFF ) {
-				return "uint16";
-			} else {
-				return "uint8";
-			}
-		}
-	}
-	return "int32";
-}
 
 function traverseSchema( map, parentPath, schema, reverseMap = false ) {
 	console.log( `traversing ${parentPath}` );
@@ -84,57 +52,19 @@ function traverseObject( output, map, parentPath, object ) {
 	const entry = map.get( parentPath );
 	if ( entry ) {
 		// TODO: write out ID according to size
-		output.buffer.writeUInt8( entry.id, output.pos );
-		output.pos += 1;
+		output.writeUInt8( entry.id );
 		switch ( entry.schema.type ) {
 		case 'string':
-			const len = Buffer.byteLength( object, 'utf8' );
-			output.buffer.writeUInt16LE( len, output.pos );
-			output.pos += 2;
-			output.buffer.write( object, output.pos, 'utf8' );
-			output.pos += len;
+			output.writeString( object );
 			break;
 		case 'integer':
-			const numberType = getNumberType( entry.schema );
-			switch( numberType ) {
-				case "uint8":
-					output.buffer.writeUInt8( object, output.pos );
-					output.pos += 1;
-					break;
-				case "uint16":
-					output.buffer.writeUInt16LE( object, output.pos );
-					output.pos += 2;
-					break;
-				case "uint32":
-					output.buffer.writeUInt32LE( object, output.pos );
-					output.pos += 4;
-					break;
-				case "int8":
-					output.buffer.writeInt8( object, output.pos );
-					output.pos += 1;
-					break;
-				case "int16":
-					output.buffer.writeInt16LE( object, output.pos );
-					output.pos += 2;
-					break;
-				case "int32":
-					output.buffer.writeInt32LE( object, output.pos );
-					output.pos += 4;
-					break;
-			}
+			output.writeInteger( object, entry.schema.minimum, entry.schema.maximum );
 			break;
 		case 'number':
-			if ( shouldUseFloat( entry.schema ) ) {
-				output.buffer.writeFloatLE( object, output.pos );
-				output.pos += 4;
-			} else {
-				output.buffer.writeDoubleLE( object, output.pos );
-				output.pos += 8;
-			}
+			output.writeNumber( object, entry.schema.minimum, entry.schema.maximum );
 			break;
 		case 'boolean':
-			output.buffer.writeUInt8( object ? 1 : 0, output.pos );
-			output.pos += 1;
+			output.writeBoolean( object );
 			break;
 		}
 	} else {
@@ -157,12 +87,10 @@ function compress( schema, object ) {
 	const map = new Map();
 	traverseSchema( map, '', schema, false );
 	console.log( `map ${JSON.stringify(Array.from( map.keys() ))}` );
-	const buffer = Buffer.alloc( 32 );
-	const output = { buffer, pos: 0 };
+	const output = new io.WriteBuffer();
 	traverseObject( output, map, '', object );
-	buffer.writeUInt8( 0, output.pos );
-	output.pos += 1;
-	return buffer;
+	output.writeUInt8( 0 );
+	return output.buffer;
 }
 
 function insertIntoObject( obj, path, val ) {
@@ -179,8 +107,7 @@ function insertIntoObject( obj, path, val ) {
 
 function buildObject( obj, map, input ) {
 	while ( true ) {
-		const id = input.buffer.readUInt8( input.pos );
-		input.pos += 1;
+		const id = input.readUInt8();
 		console.log( `id=${id}` );
 		const element = map.get( id );
 		if ( !element ) {
@@ -188,58 +115,20 @@ function buildObject( obj, map, input ) {
 		}
 		switch ( element.schema.type ) {
 			case 'string':
-				const len = input.buffer.readUInt16LE( input.pos );
-				input.pos += 2;
-				const str = input.buffer.toString( 'utf8', input.pos, input.pos + len );
-				input.pos += len;
+				const str = input.readString();
 				insertIntoObject( obj, element.path, str );
 				break;
 			case 'integer':
-				const numberType = getNumberType( element.schema );
-				let integer;
-				switch( numberType ) {
-					case "uint8":
-						integer = input.buffer.readUInt8( input.pos );
-						input.pos += 1;
-						break;
-					case "uint16":
-						integer = input.buffer.readUInt16LE( input.pos );
-						input.pos += 2;
-						break;
-					case "uint32":
-						integer = input.buffer.readUInt32LE( input.pos );
-						input.pos += 4;
-						break;
-					case "int8":
-						integer = input.buffer.readInt8( input.pos );
-						input.pos += 1;
-						break;
-					case "int16":
-						integer = input.buffer.readInt16LE( input.pos );
-						input.pos += 2;
-						break;
-					case "int32":
-						integer = input.buffer.readInt32LE( input.pos );
-						input.pos += 4;
-						break;
-				}
+				const integer = input.readInteger( element.schema.minimum, element.schema.maximum );
 				insertIntoObject( obj, element.path, integer );
 				break;
 			case 'number':
-				let num;
-				if ( shouldUseFloat( element.schema ) ) {
-					num = input.buffer.readFloatLE( input.pos );
-					input.pos += 4;
-				} else {
-					num = input.buffer.readDoubleLE( input.pos );
-					input.pos += 8;
-				}
+				const num = input.readNumber( element.schema.minimum, element.schema.maximum );
 				insertIntoObject( obj, element.path, num );
 				break;
 			case 'boolean':
-				const boo = input.buffer.readUInt8( input.pos );
-				input.pos += 1;
-				insertIntoObject( obj, element.path, boo > 0 );
+				const boo = input.readBoolean();
+				insertIntoObject( obj, element.path, boo );
 				break;
 		}
 	}
@@ -249,7 +138,8 @@ function decompress( schema, buffer ) {
 	const map = new Map();
 	traverseSchema( map, '', schema, true );
 	const obj = {};
-	buildObject( obj, map, { buffer, pos: 0 } );
+	const input = new io.ReadBuffer( buffer );
+	buildObject( obj, map, input );
 	return obj;
 }
 
---
+++ io.js
@@ -0,0 +1,252 @@
+const limits = require( './limits.js' );
+
+function shouldUseFloat( minimum, maximum ) {
+	if ( typeof minimum === 'number' && typeof maximum === 'number' ) {
+		return minimum >= limits.FLT_MIN && maximum <= limits.FLT_MAX;
+	}
+	return false;
+}
+
+function getNumberType( minimum, maximum ) {
+	if ( typeof minimum === 'number' && typeof maximum === 'number' ) {
+		// TODO: store ranges instead of integers to reduce sizes further
+		if ( minimum < 0 ) {
+			// signed
+			if ( minimum >= -0x7F && maximum <= 0x7F ) {
+				return "int8";
+			} else if ( minimum >= -0x7FFF && maximum <= 0x7FFF ) {
+				return "int16";
+			} else {
+				return "int32";
+			}
+		} else {
+			// unsigned
+			if ( maximum > 0xFFFF ) {
+				return "uint32";
+			} else if ( maximum > 0xFF ) {
+				return "uint16";
+			} else {
+				return "uint8";
+			}
+		}
+	}
+	return "int32";
+}
+
+
+class WriteBuffer {
+	constructor() {
+		this._bigEndian = false;
+		this._buffer = Buffer.alloc( 32 );
+		this._pos = 0;
+	}
+	
+	get buffer() {
+		return this._buffer;
+	}
+	
+	get position() {
+		return this._pos;
+	}
+	
+	set position( val ) {
+		this._pos = val;
+	}
+	
+	writeUInt8( num ) {
+		this._buffer.writeUInt8( num, this._pos );
+		this._pos += 1;
+	}
+	
+	writeNumber( num, minimum, maximum ) {
+		if ( shouldUseFloat( minimum, maximum ) ) {
+			if ( !this._bigEndian ) {
+				this._buffer.writeFloatLE( num, this._pos );
+			} else {
+				this._buffer.writeFloatBE( num, this._pos );
+			}
+			this._pos += 4;
+		} else {
+			if ( !this._bigEndian ) {
+				this._buffer.writeDoubleLE( num, this._pos );
+			} else {
+				this._buffer.writeDoubleBE( num, this._pos );
+			}
+			this._pos += 8;
+		}
+	}
+	
+	writeInteger( num, minimum, maximum ) {
+		switch( getNumberType( minimum, maximum ) ) {
+			case "uint8":
+				this._buffer.writeUInt8( num, this._pos );
+				this._pos += 1;
+				break;
+			case "uint16":
+				if ( !this._bigEndian ) {
+					this._buffer.writeUInt16LE( num, this._pos );
+				} else {
+					this._buffer.writeUInt16BE( num, this._pos );
+				}
+				this._pos += 2;
+				break;
+			case "uint32":
+				if ( !this._bigEndian ) {
+					this._buffer.writeUInt32LE( num, this._pos );
+				} else {
+					this._buffer.writeUInt32BE( num, this._pos );
+				}
+				this._pos += 4;
+				break;
+			case "int8":
+				this._buffer.writeInt8( num, this._pos );
+				this._pos += 1;
+				break;
+			case "int16":
+				if ( !this._bigEndian ) {
+					this._buffer.writeInt16LE( num, this._pos );
+				} else {
+					this._buffer.writeInt16BE( num, this._pos );
+				}
+				this._pos += 2;
+				break;
+			case "int32":
+				if ( !this._bigEndian ) {
+					this._buffer.writeInt32LE( num, this._pos );
+				} else {
+					this._buffer.writeInt32BE( num, this._pos );
+				}
+				this._pos += 4;
+				break;
+		}
+	}
+	
+	writeString( str ) {
+		const len = Buffer.byteLength( str, 'utf8' );
+		if ( !this._bigEndian ) {
+			this._buffer.writeUInt16LE( len, this._pos );
+		} else {
+			this._buffer.writeUInt16BE( len, this._pos );
+		}
+		this._pos += 2;
+		this._buffer.write( str, this._pos, 'utf8' );
+		this._pos += len;
+	}
+	
+	writeBoolean( boo ) {
+		this._buffer.writeUInt8( boo ? 1 : 0, this._pos );
+		this._pos += 1;
+	}
+}
+
+
+
+class ReadBuffer {
+	constructor( buffer ) {
+		this._bigEndian = false;
+		this._buffer = buffer;
+		this._pos = 0;
+	}
+	
+	get position() {
+		return this._pos;
+	}
+	
+	set position( val ) {
+		this._pos = val;
+	}
+	
+	readUInt8() {
+		const num = this._buffer.readUInt8( this._pos );
+		this._pos += 1;
+		return num;
+	}
+	
+	readNumber( minimum, maximum ) {
+		const pos = this._pos;
+		if ( shouldUseFloat( minimum, maximum ) ) {
+			this._pos += 4;
+			if ( !this._bigEndian ) {
+				return this._buffer.readFloatLE( pos );
+			} else {
+				return this._buffer.readFloatBE( pos );
+			}
+		} else {
+			this._pos += 8;
+			if ( !this._bigEndian ) {
+				return this._buffer.readDoubleLE( pos );
+			} else {
+				return this._buffer.readDoubleBE( pos );
+			}
+		}
+	}
+	
+	readInteger( minimum, maximum ) {
+		const pos = this._pos;
+		switch( getNumberType( minimum, maximum ) ) {
+			case "uint8":
+				this._pos += 1;
+				return this._buffer.readUInt8( pos );
+				break;
+			case "uint16":
+				this._pos += 2;
+				if ( !this._bigEndian ) {
+					return this._buffer.readUInt16LE( pos );
+				} else {
+					return this._buffer.readUInt16BE( pos );
+				}
+				break;
+			case "uint32":
+				this._pos += 4;
+				if ( !this._bigEndian ) {
+					return this._buffer.readUInt32LE( pos );
+				} else {
+					return this._buffer.readUInt32BE( pos );
+				}
+				break;
+			case "int8":
+				this._pos += 1;
+				return this._buffer.readInt8( pos );
+				break;
+			case "int16":
+				this._pos += 2;
+				if ( !this._bigEndian ) {
+					return this._buffer.readInt16LE( pos );
+				} else {
+					return this._buffer.readInt16BE( pos );
+				}
+				break;
+			case "int32":
+				this._pos += 4;
+				if ( !this._bigEndian ) {
+					return this._buffer.readInt32LE( pos );
+				} else {
+					return this._buffer.readInt32BE( pos );
+				}
+				break;
+		}
+		return 0;
+	}
+	
+	readString() {
+		let len;
+		if ( !this._bigEndian ) {
+			len = this._buffer.readUInt16LE( this._pos );
+		} else {
+			len = this._buffer.readUInt16BE( this._pos );
+		}
+		this._pos += 2;
+		const str = this._buffer.toString( 'utf8', this._pos, this._pos + len );
+		this._pos += len;
+		return str;
+	}
+	
+	readBoolean() {
+		const b = this._buffer.readUInt8( this._pos ) > 0;
+		this._pos += 1;
+		return b;
+	}
+}
+
+exports.WriteBuffer = WriteBuffer;
+exports.ReadBuffer = ReadBuffer;
