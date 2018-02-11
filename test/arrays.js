cc9982662387d8ec9c4f4abd6bbc7c200d315ae6
Pete Ward <peteward44@gmail.com>
2018-02-11 01:12:24 +0000

    got arrays working

--- test/arrays.js
+++ test/arrays.js
@@ -1,28 +1,234 @@
 const execute = require( './execute.js' );
-const limits = require( '../lib/limits.js' );
 
-const schema = {
-	"type": "object",
-	"properties": {
-		"test": {
-			"type": "array",
-			"items": {
-				"type": "integer",
-				"minimum": 0
-			}
-		}
-	}
-};
-
-const tests = {
+execute( 'Arrays', {
 	"empty": {
-		schema,
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "integer",
+						"minimum": 0
+					}
+				}
+			}
+		},
 		inputs: {
 			"empty": {
 				test: []
 			}
 		}
 	}
-};
+} );
 
-execute.only( 'Array tests', tests );
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "array"
+					}
+				}
+			}
+		},
+		inputs: {
+			"empty multi-dimensional": {
+				test: [
+					[]
+				]
+			}
+		}
+	}
+} );
+
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "array"
+					}
+				}
+			}
+		},
+		inputs: {
+			"empty multi-dimensional multiple entries": {
+				test: [
+					[],
+					[],
+					[]
+				]
+			}
+		}
+	}
+} );
+
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "object"
+					}
+				}
+			}
+		},
+		inputs: {
+			"single empty object": {
+				test: [
+					{}
+				]
+			}
+		}
+	}
+} );
+
+
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "object"
+					}
+				}
+			}
+		},
+		inputs: {
+			"multiple empty object": {
+				test: [
+					{},
+					{},
+					{}
+				]
+			}
+		}
+	}
+} );
+
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "array",
+						"items": {
+							"type": "object"
+						}
+					}
+				}
+			}
+		},
+		inputs: {
+			"single empty object in multi-dimensional array": {
+				test: [
+					[
+						{}
+					]
+				]
+			}
+		}
+	}
+} );
+
+
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "array",
+						"items": {
+							"type": "object"
+						}
+					}
+				}
+			}
+		},
+		inputs: {
+			"single empty object in multi-dimensional array": {
+				test: [
+					[
+						{}
+					],
+					[
+						{}
+					],
+					[
+						{}
+					]
+				]
+			}
+		}
+	}
+} );
+
+
+execute( 'Arrays', {
+	"empty": {
+		schema: {
+			"type": "object",
+			"properties": {
+				"test": {
+					"type": "array",
+					"items": {
+						"type": "object",
+						"properties": {
+							"array": {
+								"type": "array",
+								"items": {
+									"type": "object",
+									"properties": {
+										"num": {
+											"type": "integer"
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		inputs: {
+			"array deep in object within another array": {
+				test: [
+					{
+						array: [
+							{
+								num: 100
+							}
+						]
+					},
+					{
+						array: [
+							{
+								num: 50
+							}
+						]
+					}
+				]
+			}
+		}
+	}
+} );
+
